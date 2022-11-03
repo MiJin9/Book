4bdd5abda17d613d04e27692ce08ef2701833663
MIJIN KIM <92290030+MiJin9@users.noreply.github.com>
2022-11-03 13:43:20 +0900

    Create AppleRedAndHeavyPredicate.java

---
+++ 모던 자바 인 액션/실습/AppleRedAndHeavyPredicate.java
@@ -0,0 +1,5 @@
+public class AppleRedAndHeavyPredicate implements ApplePredicate {
+    public boolean test(Apple apple) {
+        return "red".equals(apple.getColor()) && apple.getWeight() > 150;
+    }
+}
